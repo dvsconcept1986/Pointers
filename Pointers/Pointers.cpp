111e325c78f884413146e1491faac765167832b7
dvsconcept1986 <ejfernandez@student.fullsail.edu>
2024-04-14 22:38:08 -0400

    Completed Program 2 of Lab 2.

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -5,9 +5,33 @@
 #include <cstdlib>
 #include <ctime>
 
-void RandomAddress(int numArr[]);
+void RandomAddress();
+void PrintIntAndAddress(int value, int* ptr);
 
 int main()
+{
+    //Program 1 start
+    RandomAddress();
+    std::cout << "\n";
+    //Program 1 end
+
+    //Program 2 start
+    int numberArray[15];
+
+    for (int i = 0; i < 15; ++i)
+        numberArray[i] = std::rand() % 100 + 1;
+
+    std::cout << "Array Values and corresponding Memory Address:\n";
+    std::cout << "==============================================\n";
+    for (int i = 0; i < 15; ++i)
+        PrintIntAndAddress(numberArray[i], &numberArray[i]);
+    //Program 2 end
+
+
+
+}
+
+void RandomAddress()
 {
     int numberArray[15];
 
@@ -17,14 +41,14 @@ int main()
 
     for (int i = 0; i < 15; ++i)
         std::cout << numberArray[i] << "\n";
-    
+
     std::cout << "\n";
     std::cout << "Address of the numbers in the array:\n";
-    RandomAddress(numberArray);
+    std::cout << "====================================\n";
+    for (int i = 0; i < 15; ++i)
+        std::cout << &numberArray[i] << "\n";
 }
 
-void RandomAddress(int numArr[])
-{
-    for (int i = 0; i < 15; ++i)
-        std::cout << &numArr[i] << "\n";
+void PrintIntAndAddress(int value, int* ptr) {
+    std::cout << "Value: " << value << " Memory Address: " << ptr << "\n";
 }
