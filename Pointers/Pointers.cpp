c0c51754403256277a2b429e53dfc539bf5ce46a
dvsconcept1986 <ejfernandez@student.fullsail.edu>
2024-04-14 22:27:36 -0400

    Finished program 1 of Lab 2.

--- Pointers/Pointers.cpp
+++ Pointers/Pointers.cpp
@@ -2,10 +2,29 @@
 //
 
 #include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+void RandomAddress(int numArr[]);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int numberArray[15];
+
+    for (int i = 0; i < 15; ++i)
+        numberArray[i] = std::rand() % 100 + 1;
+
+
+    for (int i = 0; i < 15; ++i)
+        std::cout << numberArray[i] << "\n";
+    
+    std::cout << "\n";
+    std::cout << "Address of the numbers in the array:\n";
+    RandomAddress(numberArray);
 }
 
-
+void RandomAddress(int numArr[])
+{
+    for (int i = 0; i < 15; ++i)
+        std::cout << &numArr[i] << "\n";
+}
